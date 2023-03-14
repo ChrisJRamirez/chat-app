eea152b057a2780bbe5e3785f5c0ab44e207d077
ChrisJRamirez <chrisjamesramirez@gmail.com>
2023-03-14 13:01:23 -0400

    added api in client folder, build first post mutation

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import { Configuration, OpenAIApi } from "openai";
+import openAiRoutes from "./routes/openai.js";
 
 // Configurations
 dotenv.config();
@@ -23,6 +24,10 @@ const configuration = new Configuration({
   apiKey: process.env.OPEN_API_KEY,
 });
 export const openai = new OpenAIApi(configuration);
+
+
+// Routes
+app.use("/openai", openAiRoutes)
 
 
 // Server Setup
